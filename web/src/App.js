58b69d375f93c946cac3880ca9c507acf31368b3
sathvik <sathviknarasimha@gmail.com>
2023-12-29 17:01:08 +0530

    pooja

--- web/src/App.js
+++ web/src/App.js
@@ -5,16 +5,18 @@ import HomePage from "./pages/HomePage";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import NavBar from "./components/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      {/* <NavBar />
       <Routes>
         <Route path={routes.homePage} element={<HomePage />} />
         <Route path={routes.about} element={<About />} />
         <Route path={routes.contact} element={<Contact />} />
-      </Routes>
+      </Routes> */}
+      <Contact/>
     </div>
   );
 }
