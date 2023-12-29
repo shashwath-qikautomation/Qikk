8a43ba6a258dcc3392c2599fe273708a50037e2d
shashwath-qikautomation <shashwath@qikautomation.com>
2023-12-29 17:07:59 +0530

    ramya

--- web/src/App.js
+++ web/src/App.js
@@ -5,16 +5,20 @@ import HomePage from "./pages/HomePage";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import NavBar from "./components/NavBar";
+import Blog from "./pages/Blog";
+import "bootstrap/dist/css/bootstrap.min.css"
+
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      {/* <NavBar />
       <Routes>
         <Route path={routes.homePage} element={<HomePage />} />
         <Route path={routes.about} element={<About />} />
         <Route path={routes.contact} element={<Contact />} />
-      </Routes>
+      </Routes> */}
+      <Blog />
     </div>
   );
 }
